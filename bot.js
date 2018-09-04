2a1be90522703709f58240e0b02817bbf15cd6ec
r3bteam <39262475+r3bteam@users.noreply.github.com>
2018-09-04 23:46:43 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,64 @@
+const Discord = require("discord.js");
+const RichEmbed = require("discord.js");
+const { Client, Util } = require('discord.js');
+const client = new Discord.Client();
+
+
+client.on('message',async message => {
+    var prefix = "!";
+    if(message.content.startsWith(prefix + "obc")) {
+      if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
+      let filter = m => m.author.id === message.author.id;
+      let thisMessage;
+      let thisFalse;
+      message.channel.send(':regional_indicator_b::regional_indicator_c:| **ارسل الرسالة الان**').then(msg => {
+  
+      let awaitM = message.channel.awaitMessages(filter, {
+        max: 1,
+        time: 20000,
+        errors: ['time']
+      })
+      .then(collected => {
+        collected.first().delete();
+        thisMessage = collected.first().content;
+        msg.edit(':regional_indicator_b::regional_indicator_c:| **هل انت متأكد؟**');
+        let awaitY = message.channel.awaitMessages(response => response.content === 'نعم' || 'لا' && filter,{
+          max: 1,
+          time: 20000,
+          errors: ['time']
+        })
+        .then(collected => {
+          if(collected.first().content === 'لا') {
+            msg.delete();
+            message.delete();
+            thisFalse = false;
+          }
+          if(collected.first().content === 'نعم') {
+            if(thisFalse === false) return;
+          message.guild.members.forEach(member => {
+            msg.edit(':regional_indicator_b::regional_indicator_c:| **جاري الارسال**');
+            collected.first().delete();
+            member.send(`${thisMessage}\n\n${member}`);
+          });
+          }
+        });
+      });
+      });
+    }
+  });
+    
+                    client.on("message", message => {
+                         var prefix = "!";
+                        if (message.content.startsWith(prefix + "bc")) {
+                                     if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+              let args = message.content.split(" ").slice(1);
+              var argresult = args.join(' '); 
+              message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+             m.send(`${argresult}\n ${m}`);
+            })
+             message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+             message.delete(); 
+            };     
+            });
+
+            client.login(process.env.BOT_TOKEN);
